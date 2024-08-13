2272620de74f4a326d30a52127b86c1871eaf627
mbeka02 <antonymbeka@gmail.com>
2024-08-13 14:28:27 +0300

    added base

---
+++ src/base.ts
@@ -0,0 +1,33 @@
+type Config = {
+  apiKey: string;
+  baseUrl?: string;
+};
+
+export abstract class Base {
+  private apiKey: string;
+  private baseUrl: string;
+
+  constructor(config: Config) {
+    this.apiKey = config.apiKey;
+    this.baseUrl = config.baseUrl || "";
+  }
+
+  protected invoke<T>(endpoint: string, options?: RequestInit): Promise<T> {
+    const url = this.baseUrl + endpoint;
+    const headers = {
+      "Content-Type": "application/json",
+      "api-key": this.apiKey,
+    };
+    const config = {
+      ...options,
+      headers,
+    };
+    return fetch(url, config).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+}
